cbc924bf4e846d03d9bb5d8df71e808a3e7afcef
notrbgm <mrb07011@gmail.com>
2025-04-08 05:07:48 +0300

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,16 +1,10 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-// Utility to combine class names
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-// TMDB: Get cast & crew credits
-export async function getCredits(id: string, mediaType: "movie" | "tv") {
-  const res = await fetch(
-    `https://api.themoviedb.org/3/${mediaType}/${id}/credits?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
-  );
-  if (!res.ok) throw new Error("Failed to fetch credits");
-  return res.json();
+/**
+ * Combines class names conditionally with Tailwind merge support.
+ * Useful for conditional styling in components.
+ */
+export function cn(...inputs: ClassValue[]): string {
+  return twMerge(clsx(...inputs));
 }
