b65d844c1f6d85eb9d3f1786f86aaf484dab0dea
notrbgm <mrb07011@gmail.com>
2025-04-11 23:58:11 +0300

    efs

--- src/components/TopTenRow.tsx
+++ src/components/TopTenRow.tsx
@@ -11,7 +11,7 @@ import { useState } from "react";
 import "./scrollbar-hide.css"; // Import scrollbar-hide.css
 
 interface TopTenRowProps {
-  title: React.ReactNode;
+  title: string;
   movies: Movie[];
 }
 
