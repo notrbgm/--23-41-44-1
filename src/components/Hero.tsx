1158cb85a3305b47c68ea4a23e53ac2fcf34f9fc
notrbgm <mrb07011@gmail.com>
2025-04-12 16:28:53 +0300

    wd

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,6 +7,8 @@ import { useState, useEffect, useRef, useCallback } from "react";
 import MovieDetailsModal from "./MovieDetailsModal";
 import { Image } from "./ui/image";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
+import "./Hero.css";
+
 
 const Hero = () => {
   const [selectedMovie, setSelectedMovie] = useState(null);
