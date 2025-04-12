151d5bfb3ac545ea3eb190afd59ce5fd57b5bb6a
notrbgm <mrb07011@gmail.com>
2025-04-12 18:36:48 +0300

    dw

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -116,7 +116,7 @@ const Hero: React.FC = () => {
       {/* Dots Indicator */}
       <div
         className="absolute left-[50%] transform -translate-x-[50%] flex gap-[8px]"
-        style={{ bottom: '8%' }}
+        style={{ bottom: '12%' }} // Adjusted bottom position to move dots higher
       >
         {limitedMovies.map((_, index) => (
           <button
